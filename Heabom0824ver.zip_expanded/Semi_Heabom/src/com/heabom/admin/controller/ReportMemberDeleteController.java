551151cbf0551133fefa602a37adcfe67b32463a
jangheeju-it <chwdgn01@gmail.com>
2023-09-03 22:16:45 +0900

    commit

--- Heabom0824ver.zip_expanded/Semi_Heabom/src/com/heabom/admin/controller/ReportMemberDeleteController.java
+++ Heabom0824ver.zip_expanded/Semi_Heabom/src/com/heabom/admin/controller/ReportMemberDeleteController.java
@@ -1,6 +1,8 @@
 package com.heabom.admin.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -30,6 +32,7 @@ public class ReportMemberDeleteController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		
 		String userId = request.getParameter("userId");
 		String userPwd = request.getParameter("userPwd");
