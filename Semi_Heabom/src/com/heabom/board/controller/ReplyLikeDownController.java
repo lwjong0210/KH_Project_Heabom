fc16c96990ac8d2b8f4038ec4dff95918d53a172
wj <lwjong99@gmail.com>
2023-09-06 14:20:16 +0900

    원종 댓글

--- Semi_Heabom/src/com/heabom/board/controller/ReplyLikeDownController.java
+++ Semi_Heabom/src/com/heabom/board/controller/ReplyLikeDownController.java
@@ -1,30 +1,26 @@
 package com.heabom.board.controller;
 
 import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import com.heabom.board.model.service.BoardService;
-import com.heabom.member.model.vo.Member;
 import com.heabom.place.model.service.PlaceService;
 
 /**
- * Servlet implementation class BoardLikeUpController
+ * Servlet implementation class ReplyLikeDownController
  */
-@WebServlet("/likeup.bo")
-public class BoardLikeUpController extends HttpServlet {
+@WebServlet("/likedown.bo")
+public class ReplyLikeDownController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public BoardLikeUpController() {
+    public ReplyLikeDownController() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -43,13 +39,13 @@ public class BoardLikeUpController extends HttpServlet {
 		BoardService bservice = new BoardService();
 		
 		// 먼저 댓글번호를 가지고 가서 리뷰 리플라이 테이블 like 를 증가시킨다
-		bservice.replyLikeUp(rpno);
+		bservice.replyLikeDown(rpno);
 		
 		// 좋아요 몇개인지 가져온다
 		int replyLikeCount = new BoardService().replyLikeCount(rpno);
 		
 		// tb_like로가서 로그인멤버와 댓글 번호 업데이트
-		bservice.updateLike(loginMem, rpno);
+		bservice.deleteLike(loginMem, rpno);
 		
 		// 등급업데이트
 		if(!writer.equals("M1")) {
