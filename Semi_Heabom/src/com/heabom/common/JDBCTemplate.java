990027c4f8cdf895d5dba01eeb0adaa4ebfaa3e0
조준하 <48859949+whwnsgk@users.noreply.github.com>
2023-09-30 21:35:55 +0900

    123

--- Semi_Heabom/src/com/heabom/common/JDBCTemplate.java
+++ Semi_Heabom/src/com/heabom/common/JDBCTemplate.java
@@ -1,54 +1,74 @@
 package com.heabom.common;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
 
 public class JDBCTemplate {
-
+	//1. Connetcion 객체 생성 한후 해당 Connection 객체를 반환시켜주는 getConnction 메서드
 	
 	public static Connection getConnection() {
+		// jdbc driver 등록 
+		// 붙어먹고자하는 db의 url, 계정명 , 비밀번호를 제시해서 Connection 객체를 생성
+		Connection conn = null ; //커넥션 객체 생성을 해야겠지
 		
-		Connection conn = null;
-		Properties prop = new Properties();
+		Properties prop = new Properties(); //MAP 계열 collection key => value
+		//읽어들이고자 하는 classes 폴더내에 driver.properties 파일의 파일 경로를 만들어줘야함
+		String filepath = JDBCTemplate.class.getResource("/db/driver/driver.properties").getPath();
+		//"c:05_server-workspace2/jspProject/webContent/classes/db/driver/driver-properties";
+		//클래스파일의 경로를 찾아주는 .class.메서드
+		//c:05_server-workspace2/jspProject/webContent/classes/db/driver/driver-properties
 		
-		String filePath = JDBCTemplate.class.getResource("/db/driver/driver.properties").getPath();
+		
+		
+		
+		
 		try {
-			prop.load(new FileInputStream(filePath));
+			prop.load(new FileInputStream(filepath)); // prop 에 경로 값 주기 
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
 		
 		
-		try {
-			Class.forName(prop.getProperty("driver")); // ����
-			
-			conn = DriverManager.getConnection(prop.getProperty("url"),      // ����
-											   prop.getProperty("username"), // ����
-											   prop.getProperty("password"));// ����
-			
-			conn.setAutoCommit(false);
-			
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
 		
-		return conn;
-		
+			try {
+				Class.forName(prop.getProperty("driver")); 
+				
+				
+				conn = DriverManager.getConnection(prop.getProperty("url"),
+						prop.getProperty("username"),
+						prop.getProperty("password")); // "이건 걍 외워;",계정명, 비밀번호
+				
+				//oracle클래스를 쓰는거기때문에 web-inf 안에 ojdbc6 을 꼭 넣어주도록 하자
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			return conn; 
+	
 	}
 	
-	// 2-1. Connection ��ü ���޹޾Ƽ� commit �����ִ� commit �޼ҵ�
+	
+	
+	
+	
+	
+	
+	
+	
+	//2. Connection 객체 전달 받아서 commit 시켜주는 메서드 만들기
 	public static void commit(Connection conn) {
 		try {
-			if(conn != null && !conn.isClosed()) {
+			if (conn != null && !conn.isClosed()) {
 				conn.commit();
 			}
 		} catch (SQLException e) {
@@ -56,7 +76,9 @@ public class JDBCTemplate {
 		}
 	}
 	
-	// 2-2. 	"	   ��ü�� ���޹޾Ƽ� rollback �����ִ� rollback �޼ҵ�
+
+	
+	//2.2 rollback 시켜주는 메서드
 	public static void rollback(Connection conn) {
 		try {
 			if(conn != null && !conn.isClosed()) {
@@ -67,10 +89,13 @@ public class JDBCTemplate {
 		}
 	}
 	
-	// 3-1.     "      ��ü�� ���޹޾Ƽ� �ݳ������ִ� close �޼ҵ�
+	
+	
+	
+	//3-1 Connection 객체를 전달 받아서 반납시켜주는 close 메서드
 	public static void close(Connection conn) {
 		try {
-			if(conn != null && !conn.isClosed()) {
+			if(conn!= null && !conn.isClosed()) {
 				conn.close();
 			}
 		} catch (SQLException e) {
@@ -78,7 +103,10 @@ public class JDBCTemplate {
 		}
 	}
 	
-	// 3-2. Statement  ��ü�� ���޹޾Ƽ� �ݳ������ִ� close �޼ҵ�
+	
+	
+	//3-2 Statement 객체 전달 받아서 반납시켜주는 close 메서드
+	
 	public static void close(Statement stmt) {
 		try {
 			if(stmt != null && !stmt.isClosed()) {
@@ -89,10 +117,14 @@ public class JDBCTemplate {
 		}
 	}
 	
-	// 3-3. ResultSet  ��ü�� ���޹޾Ƽ� �ݳ������ִ� close �޼ҵ�
+	
+	
+	
+	//3-3 ResultSet 객체 전달 받아서 반납시켜주는 close 메서드 
+	
 	public static void close(ResultSet rset) {
 		try {
-			if(rset != null && !rset.isClosed()) {
+			if (rset != null && !rset.isClosed()) {
 				rset.close();
 			}
 		} catch (SQLException e) {
@@ -100,32 +132,4 @@ public class JDBCTemplate {
 		}
 	}
 	
-	
-
-	
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
